b6fd03f801ae590c341bfc038724398c75292df9
〽️ David <146224919+davidlreg@users.noreply.github.com>
2025-12-23 18:35:12 +0100

    added main class

--- src/reisebuero/app.java
+++ src/reisebuero/app.java
@@ -2,4 +2,9 @@ package reisebuero;
 
 public class App {
 
+public static void main(String[] args) {
+    System.out.println("Willkommen im Reisebüro!");
+    System.out.println("Lade Reiseangebote...");
+}
+
 }
