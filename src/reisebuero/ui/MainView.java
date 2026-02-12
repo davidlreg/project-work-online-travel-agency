725543e55303f2c0344d03b27901ca0497a14032
〽️ David <146224919+davidlreg@users.noreply.github.com>
2026-02-12 19:36:01 +0100

    Implemented Basic Menu with all necessary functions of the programm

--- src/reisebuero/ui/MainView.java
+++ src/reisebuero/ui/MainView.java
@@ -1,5 +1,24 @@
 package reisebuero.ui;
 
+import java.util.Scanner;
+
 public class MainView {
+
+    private Scanner scanner = new Scanner(System.in);
+
+    // Zeigt das Hauptmenü und gibt die Eingabe zurück
+    public String showMenu() {
+
+        System.out.println("Menü:");
+        System.out.println("1 - Alle Reiseangebote anzeigen");
+        System.out.println("2 - Reiseangebote filtern");
+        System.out.println("3 - Warenkorb anzeigen");
+        System.out.println("4 - Buchung abschließen");
+        System.out.println("5 - Hilfe");
+        System.out.println("0 - Programm beenden");
+        System.out.print("Bitte wählen: ");
+
+        return scanner.nextLine();
+    }
 
 }
