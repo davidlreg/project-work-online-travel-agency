c0f4082eb2b53a623f61d92243cd3ee830ab23c9
〽️ David <146224919+davidlreg@users.noreply.github.com>
2026-02-12 19:36:52 +0100

    Implemented Class to Display all availabel traveldata in the console

--- src/reisebuero/ui/TravelListView.java
+++ src/reisebuero/ui/TravelListView.java
@@ -1,5 +1,22 @@
 package reisebuero.ui;
 
+import reisebuero.model.Travel;
+import java.util.List;
+
 public class TravelListView {
+
+     // Zeigt alle Reisen in der Konsole an
+    public void showTravels(List<Travel> travels) {
+
+        System.out.println();
+        System.out.println("Verfügbare Reisen:");
+        System.out.println("-------------------");
+
+        for (int i = 0; i < travels.size(); i++) {
+            System.out.println((i + 1) + ". " + travels.get(i));
+        }
+
+        System.out.println();
+    }
 
 }
