2c156cf34c304828c58ad6b4ef11582f04a5994c
〽️ David <146224919+davidlreg@users.noreply.github.com>
2026-02-12 19:37:13 +0100

    Implemented App.java to controll the basic funktions of my programm

--- src/reisebuero/app.java
+++ src/reisebuero/app.java
@@ -1,10 +1,41 @@
 package reisebuero;
 
+import reisebuero.data.CsvTravelLoader;
+import reisebuero.model.Travel;
+import reisebuero.ui.MainView;
+import reisebuero.ui.TravelListView;
+
+import java.util.List;
+
 public class App {
 
-public static void main(String[] args) {
-    System.out.println("Willkommen im Reisebüro!");
-    System.out.println("Lade Reiseangebote...");
-}
+    public static void main(String[] args) {
 
+        CsvTravelLoader loader = new CsvTravelLoader();
+        MainView mainView = new MainView();
+        TravelListView travelView = new TravelListView();
+
+        boolean running = true;
+
+        while (running) {
+
+            String choice = mainView.showMenu();
+
+            if (choice.equals("1")) {
+
+                List<Travel> travels =
+                        loader.loadTravels("resources/traveldata.csv");
+
+                travelView.showTravels(travels);
+
+            } else if (choice.equals("0")) {
+
+                running = false;
+
+            } else {
+
+                System.out.println("Ungültige Eingabe.");
+            }
+        }
+    }
 }
