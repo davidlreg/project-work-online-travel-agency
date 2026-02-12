6d484ead73b954459aaf7d2dd3becb9d17124343
〽️ David <146224919+davidlreg@users.noreply.github.com>
2026-02-12 15:55:26 +0100

    added class to create seperate objects for each set of traveldata

--- src/reisebuero/model/Travel.java
+++ src/reisebuero/model/Travel.java
@@ -1,5 +1,29 @@
 package reisebuero.model;
 
+// Repräsentiert ein einzelnes Reiseangebot
 public class Travel {
 
+    // Eigenschaften der Reise
+    public String date;
+    public String start;
+    public String destination;
+    public String route;
+    public double price;
+
+    // Konstruktor zum Erstellen einer neuen Reise
+    public Travel(String date, String start, String destination, String route, double price) {
+        this.date = date;
+        this.start = start;
+        this.destination = destination;
+        this.route = route;
+        this.price = price;
+    }
+
+    // Legt fest, wie die Reise in der Konsole angezeigt wird
+    @Override
+    public String toString() {
+        return date + " | " + start + " -> " + destination +
+                " | Route: " + route +
+                " | Preis: " + price + " EUR";
+    }
 }
